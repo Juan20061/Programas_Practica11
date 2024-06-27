72264eb4498c6c6ad1ab81da09219a924f7d6aef
Alexis <Alexis@DESKTOP-C5RDDHT>
2024-06-27 10:08:28 -0600

    Se creo el codigo de gasolineria xml.cs

--- Programas3716370/Gasolinera.xaml.cs
+++ Programas3716370/Gasolinera.xaml.cs
@@ -2,8 +2,35 @@ namespace Programas3716370;
 
 public partial class Gasolinera : ContentPage
 {
-	public Gasolinera()
+    //Este es el precio de la gasolina por litro 
+    private const double PrecioPorLitro = 1.11;
+    // Este es el factor de conversión de galones a litros
+    private const double GalonesALitros = 3.78541; 
+
+    public Gasolinera()
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        double galones;
+        double peso;
+
+        if (double.TryParse(entry1.Text, out galones) && double.TryParse(entry2.Text, out peso))
+        {
+            //Convertimos los galones a litros
+            double litros = galones * GalonesALitros;
+
+            //Calculamos el precio total
+            double precioTotal = litros * PrecioPorLitro;
+
+            //Mostramos el resultado 
+            entry3.Text = $"${precioTotal:F2}";
+        }
+        else
+        {
+            DisplayAlert("Hubo un error al ingresar datos", "Por favor ingresa los valores correspondientes.", "Continuar");
+        }
+    }
 }
