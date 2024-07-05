89101be096c45655f29146d4e8b5cddd6dc7c5a3
Juan Carlos <Juan Carlos@DESKTOP-QQANMR9>
2024-07-05 10:56:14 -0600

    Se realizo la documentacion en el archivo Cambio.xaml.cs

--- Programas3716370/Cambio.xaml.cs
+++ Programas3716370/Cambio.xaml.cs
@@ -7,11 +7,17 @@ public partial class Cambio : ContentPage
 	{
 		InitializeComponent();
 	}
-
+	/// <summary>
+	/// Evento clicked que retorna el dinero sobrante al comprar un producto dependiento el costo y dinero dado
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
     private void Button_Clicked(object sender, EventArgs e)
     {
+		//condicionador que pide se ingrese datos en todos los entry
 		if(!string.IsNullOrEmpty(entry1.Text)&& !string.IsNullOrEmpty(entry2.Text))
 		{
+			//variables que contendran el dinero y costo el cambio
 			double cambio, dinero, costo;
 
 			dinero=Convert.ToDouble(entry1.Text);
@@ -19,6 +25,7 @@ public partial class Cambio : ContentPage
 			cambio = dinero - costo;
 			entry3.Text = "Su cambio es de "+ cambio.ToString() + "$";
 
+			//condicionador que indica que el dinero dado debe ser igual o mayor al costo del producto de lo contrario enviara un mensaje
             if (dinero < costo)
             {
                 DisplayAlert("ERROR", "El dinero dado es menor que el costo del articulo", "ok");
