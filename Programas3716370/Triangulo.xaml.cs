5ff8fcc68ac298c28224bd3f5fb4c720e48f59b1
Juan Carlos <Juan Carlos@DESKTOP-QQANMR9>
2024-07-05 11:02:21 -0600

    Se realizo la documentacion en el Triangulo.xaml.cs

--- Programas3716370/Triangulo.xaml.cs
+++ Programas3716370/Triangulo.xaml.cs
@@ -6,17 +6,23 @@ public partial class Triangulo : ContentPage
 	{
 		InitializeComponent();
 	}
-
+    /// <summary>
+    /// Evento clicked que muestra la superficie de un tringualo dependiendo 
+    /// la base y altura  este resultado se muestra por medio de un label
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void Button_Clicked(object sender, EventArgs e)
     {
-
+        //condicionador que valida si los entry estan vacios o completos
         if (!string.IsNullOrEmpty(entry1.Text) && !string.IsNullOrEmpty(entry2.Text))
-        {            
+        {    
+            //variables que contiene cada entry y el proceso que tiene almacenado
                 double baseTriangulo = Convert.ToDouble(entry1.Text);
                 double alturaTriangulo = Convert.ToDouble(entry2.Text);
 
                 double superficie = (baseTriangulo * alturaTriangulo) / 2;
-
+            //label que muestra el resultado
                 labelResultado.Text = $"La superficie del triángulo es: {superficie}";
         }
         else
