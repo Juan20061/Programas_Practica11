fab54941d82bc900f7395520fa6acce813bffa41
Juan Carlos <Juan Carlos@DESKTOP-QQANMR9>
2024-07-05 11:18:11 -0600

    Se finaliza la documentacion del todo el programa.

--- Programas3716370/App.xaml.cs
+++ Programas3716370/App.xaml.cs
@@ -3,12 +3,12 @@
     public partial class App : Application
     {
         /// <summary>
-        /// 
+        /// Constructor que permite el inicio y funcion de la app
         /// </summary>
         public App()
         {
             InitializeComponent();
-
+            //permite la navegacion entre varios programas
             MainPage = new NavigationPage(new Menu());
         }
     }
--- Programas3716370/Menu.xaml.cs
+++ Programas3716370/Menu.xaml.cs
@@ -1,35 +1,38 @@
 namespace Programas3716370;
 
+
+
 public partial class Menu : ContentPage
 {
 	public Menu()
 	{
 		InitializeComponent();
 	}
-
+    //Evetno que te permite navegar al programa Cambio
     private async void Button_Clicked(object sender, EventArgs e)
     {
 	await Navigation.PushAsync(new Cambio());
     }
 
+    //Evetno que te permite navegar al programa Tiangulo
     private async void Button_Clicked_1(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Triangulo());
 
     }
-
+    //Evetno que te permite navegar al programa del Dinosaurio
     private async void Button_Clicked_2(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Dinosaurio());
 
     }
-
+    //Evetno que te permite navegar al programa Gasolinera
     private async void Button_Clicked_3(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Gasolinera());
 
     }
-
+    //Evetno que te permite navegar a About
     private async void Button_Clicked_4(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new About());
